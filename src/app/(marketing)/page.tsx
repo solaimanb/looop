de839cde3e286857333c47c25fb680cfe3ea4ff4
solaimanb <solaimanb.dev@gmail.com>
2025-03-18 13:37:04 +0600

    update subtitle

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/hero/Hero";
 export default function Home() {
   return (
     <main className="flex flex-col h-screen">
-      <Hero title="L-O-O-O-P" subtitle="feel the poshness" />
+      <Hero title="L-O-O-O-P" subtitle="on the poshness" />
       <div className="text-[9px] font-bold text-end w-full px-4 mt-auto">
         All right reserved &copy;
         <a href="https://github.com/solaimanb" className="ml-1" target="_blank">
