4ea51c19cf9b3f9716db37e853dd481eeba09f46
solaimanb <solaimanb.dev@gmail.com>
2025-03-18 13:33:10 +0600

    udpate layout

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex flex-col h-screen">
       <Hero title="L-O-O-O-P" subtitle="feel the poshness" />
-      <div className="text-[9px] font-bold text-end w-full px-1 mt-auto">
+      <div className="text-[9px] font-bold text-end w-full px-4 mt-auto">
         All right reserved &copy;
         <a href="https://github.com/solaimanb" className="ml-1" target="_blank">
           S010
